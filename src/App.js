c6efbd1304edf05c0c91ee320725313c7937866b
kapiljhajhria <jhajhria44@gmail.com>
2020-10-07 13:47:51 +0530

    movies component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
+import Movies from "./components/movies";
 
 function App() {
-  return <main
-      className="container">
-    <h1>Hello World</h1>
-  </main>;
+    return <main
+        className="container">
+        <h1>Hello World</h1>
+        <Movies/>
+    </main>;
 }
 
 export default App;
