3892b159d42b6dc43d44e1253fe9638046ed19dd
kapiljhajhria <jhajhria44@gmail.com>
2020-10-09 23:48:43 +0530

    typeChecking with propTypes prop-types package

--- src/components/common/pagination_component.jsx
+++ src/components/common/pagination_component.jsx
@@ -1,29 +1,38 @@
 import React from "react";
 import _ from 'lodash';
+import PropTypes from 'prop-types'
 
-export default function Pagination(props) {
+function Pagination(props) {
     //array of page numbers
 
     const {itemsCount, pageSize, currentPage} = props;
     const pagesCount = Math.ceil(itemsCount / pageSize)
     console.log(`current page is ${currentPage}`)
-    if(pagesCount===1) return null
+    if (pagesCount === 1) return null
     const pages = _.range(1, pagesCount + 1);
     return (
         <nav>
             <ul className="pagination">
                 {pages.map(page =>
-                <li
-                    key={page}
-                    className={page===currentPage?"page-item active":"page-item"}
-                    onClick={()=>props.onPageChange(page)}
-                >
-                    <a
-                    className="page-link">{page}
-                    </a>
-                </li>)}
+                    <li
+                        key={page}
+                        className={page === currentPage ? "page-item active" : "page-item"}
+                        onClick={() => props.onPageChange(page)}
+                    >
+                        <a
+                            className="page-link">{page}
+                        </a>
+                    </li>)}
 
             </ul>
         </nav>
     )
 }
+
+Pagination.propTypes = {
+    itemsCount: PropTypes.number.isRequired,
+    pageSize: PropTypes.number.isRequired,
+    currentPage: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired
+};
+export default Pagination;
