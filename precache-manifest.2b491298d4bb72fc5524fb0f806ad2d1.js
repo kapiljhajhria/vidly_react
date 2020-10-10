d99352c638d89a2b76ebc16f23e7a813d25015e9
kapiljhajhria <jhajhria44@gmail.com>
2020-10-10 19:42:54 +0000

    Deploying to gh-pages from  @ e9b4fc81dea1eb3784f6b13d154d3d9d0b6ecfa4 🚀

---
+++ precache-manifest.2b491298d4bb72fc5524fb0f806ad2d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea586e934819c4c7aaa9a53b8c0154b9",
+    "url": "/vidly_react/index.html"
+  },
+  {
+    "revision": "fce341c7ab64560ec1c4",
+    "url": "/vidly_react/static/css/2.aeb3ddf4.chunk.css"
+  },
+  {
+    "revision": "4642ed86bb2caff83ae5",
+    "url": "/vidly_react/static/css/main.c2893b68.chunk.css"
+  },
+  {
+    "revision": "fce341c7ab64560ec1c4",
+    "url": "/vidly_react/static/js/2.4f4ae0ec.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/vidly_react/static/js/2.4f4ae0ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4642ed86bb2caff83ae5",
+    "url": "/vidly_react/static/js/main.e8f3a1e0.chunk.js"
+  },
+  {
+    "revision": "dec98868dd11f3ed6a3a",
+    "url": "/vidly_react/static/js/runtime-main.c551ddc5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly_react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly_react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly_react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly_react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly_react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/*/precache-manifest.b3ae41bb94ccac382d9181bd9a326521.js"
+  "/vidly_react/precache-manifest.2b491298d4bb72fc5524fb0f806ad2d1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/*/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vidly_react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
