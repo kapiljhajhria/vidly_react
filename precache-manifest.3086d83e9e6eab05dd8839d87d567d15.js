6037407c2682e4e93ab26155537b3d3dc694fae1
kapiljhajhria <jhajhria44@gmail.com>
2020-10-10 16:54:14 +0000

    Deploying to gh-pages from  @ d3fb43a9faf8070ade8206e1dede88508c7acf05 🚀

---
+++ precache-manifest.3086d83e9e6eab05dd8839d87d567d15.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9b79f98fbfad7c7ed5187b199422464",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b91e8e7033a657f418e0",
+    "url": "/static/css/2.42a5fdae.chunk.css"
+  },
+  {
+    "revision": "07421b8f657811cdea43",
+    "url": "/static/css/main.c2893b68.chunk.css"
+  },
+  {
+    "revision": "b91e8e7033a657f418e0",
+    "url": "/static/js/2.12ebb840.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/static/js/2.12ebb840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07421b8f657811cdea43",
+    "url": "/static/js/main.143b30a6.chunk.js"
+  },
+  {
+    "revision": "4f14166897be1eceb096",
+    "url": "/static/js/runtime-main.0f793c77.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
