5163f57d4994e12c858e2e93ed28343ea9692cf8
kapiljhajhria <jhajhria44@gmail.com>
2020-10-11 15:28:29 +0530

    adding keys to elements

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import _ from 'lodash'
 
 export default class TableBody extends React.Component {
-    renderCell =(item,column)=>{
-        if(column.content) return column.content(item)
+    renderCell = (item, column) => {
+        if (column.content) return column.content(item)
 
         return _.get(item, column.path);
     }
+    createKey = (item, column) => item._id + (column.path || column.key)
+
     render() {
         const {data, columns} = this.props;
 
         return (
             <tbody>
-            {data.map(item => <tr>
+            {data.map(item => <tr key={item._id}>
                 {
-                    columns.map(column => <td>{this.renderCell(item,column)}</td>)
+                    columns.map(column => <td key={this.createKey(item, column)}>{this.renderCell(item, column)}</td>)
                 }
             </tr>)}
             </tbody>
