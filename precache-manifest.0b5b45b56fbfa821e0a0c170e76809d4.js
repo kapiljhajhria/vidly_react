1f735ca0c9862e4ac75e60e017bd737de1b470bc
kapiljhajhria <jhajhria44@gmail.com>
2020-10-13 10:46:22 +0000

    Deploying to gh-pages from  @ 4d9850999491277ba9dd5c826f39fbfe676857c0 🚀

---
+++ precache-manifest.0b5b45b56fbfa821e0a0c170e76809d4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a2e8d1513fb6d4cb492a9d4add9684b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d15830f62188ad1058a3",
+    "url": "./static/css/2.10149c91.chunk.css"
+  },
+  {
+    "revision": "f15608d5c1d89528eb55",
+    "url": "./static/css/main.527e9b7d.chunk.css"
+  },
+  {
+    "revision": "d15830f62188ad1058a3",
+    "url": "./static/js/2.8c22247b.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "./static/js/2.8c22247b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15608d5c1d89528eb55",
+    "url": "./static/js/main.35cbcd11.chunk.js"
+  },
+  {
+    "revision": "9b5f9786cfc52b542aa5",
+    "url": "./static/js/runtime-main.f6245e18.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vidly_react/precache-manifest.5c84c4e1aa9db6e8212b55823108719d.js"
+  "./precache-manifest.0b5b45b56fbfa821e0a0c170e76809d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/vidly_react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
