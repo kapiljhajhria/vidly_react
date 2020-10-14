443931852293c141b1defa82794ee7c89d011acf
kapiljhajhria <jhajhria44@gmail.com>
2020-10-14 12:17:14 +0530

    refactored input form using {...rest}

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 
 export default function Input(props) {
-    const {name,label,value,onChange,autoFocus,type,error}=props;
+    const {name,label,error,...rest}=props;
     return(
         <div className="form-group">
             <label htmlFor={name}>{label}</label>
             <input
-                autoFocus={autoFocus}
+                {...rest}
                 name={name}
                 id={name}
-                value={value}
-                onChange={onChange}
-                type={type}
                 className="form-control"/>
             {error && <div className="alert alert-danger">{error}</div>}
         </div>
